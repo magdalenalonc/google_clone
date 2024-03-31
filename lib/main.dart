6089967efeaa4829f48d547f7ede6467aa22dfba
Magdalena Lonc <magdalena.lonc@gmail.com>
2024-03-31 23:43:04 +0200

    Create responsive layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_clone/colors.dart';
+import 'package:google_clone/responsive/mobile_screen_layout.dart';
+import 'package:google_clone/responsive/responsive_layout_screen.dart';
+import 'package:google_clone/responsive/web_screen_layout.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,10 +13,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Google Clone',
-      home: Text('Google Clone App'),
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: backgroundColor,
+      ),
+      home: const ResponsiveLayoutScreen(
+        mobileScreenLayout: MobileScreenLayout(),
+        webScreenLayout: WebScreenLayout(),
+      ),
     );
   }
 }
---
+++ lib/responsive/responsive_layout_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class ResponsiveLayoutScreen extends StatelessWidget {
+  const ResponsiveLayoutScreen({
+    super.key,
+    required this.mobileScreenLayout,
+    required this.webScreenLayout,
+  });
+
+  final Widget mobileScreenLayout;
+  final Widget webScreenLayout;
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth <= 768) {
+          return mobileScreenLayout;
+        }
+        return webScreenLayout;
+      },
+    );
+  }
+}
