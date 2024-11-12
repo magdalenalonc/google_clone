70cd137decba53fcb02413120957e4f299ead55c
Magdalena Lonc <magdalena.lonc@gmail.com>
2024-11-12 17:54:30 +0100

    Add mobile screen layout & make main screen responsive

--- lib/responsive/mobile_screen_layout.dart
+++ lib/responsive/mobile_screen_layout.dart
@@ -1,13 +1,96 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_clone/colors.dart';
+import 'package:google_clone/widgets/mobile/mobile_footer.dart';
+import 'package:google_clone/widgets/search.dart';
+import 'package:google_clone/widgets/translation_buttons.dart';
+import 'package:google_clone/widgets/web/search_buttons.dart';
 
 class MobileScreenLayout extends StatelessWidget {
   const MobileScreenLayout({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Hello from Mobile!'),
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: backgroundColor,
+        elevation: 0.0,
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.menu,
+            color: Colors.grey,
+          ),
+        ),
+        title: SizedBox(
+          width: size.width * 0.34,
+          child: const DefaultTabController(
+            length: 2,
+            child: TabBar(
+              labelColor: blueColor,
+              unselectedLabelColor: Colors.grey,
+              indicatorColor: blueColor,
+              indicatorSize: TabBarIndicatorSize.tab,
+              tabs: [
+                Tab(text: 'ALL'),
+                Tab(text: 'IMAGES'),
+              ],
+            ),
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: SvgPicture.asset(
+              'assets/images/more-apps.svg',
+              colorFilter:
+                  const ColorFilter.mode(primaryColor, BlendMode.srcIn),
+            ),
+          ),
+          const SizedBox(width: 10.0),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10.0).copyWith(
+              right: 10.0,
+            ),
+            child: MaterialButton(
+              onPressed: () {},
+              color: const Color(0xff1A73EB),
+              child: const Text(
+                'Sign in',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(right: 5, left: 5),
+        child: Column(
+          children: [
+            SizedBox(height: size.height * 0.25),
+            const Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      Search(),
+                      SizedBox(height: 20),
+                      SearchButtons(),
+                      SizedBox(height: 20),
+                      TranslationButtons(),
+                    ],
+                  ),
+                  MobileFooter(),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/widgets/search.dart
+++ lib/widgets/search.dart
@@ -20,7 +20,7 @@ class Search extends StatelessWidget {
         ),
         const SizedBox(height: 20),
         SizedBox(
-          width: size.width * 0.4,
+          width: size.width > 768 ? size.width * 0.4 : size.width * 0.9,
           child: TextFormField(
             decoration: InputDecoration(
               border: const OutlineInputBorder(
