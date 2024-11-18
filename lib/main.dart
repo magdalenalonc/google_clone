83f37e4faf6193f7f1f2582d23f6db360e2b65e3
Magdalena Lonc <magdalena.lonc@gmail.com>
2024-11-18 21:41:17 +0100

    Add pagination

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:google_clone/colors.dart';
 import 'package:google_clone/responsive/mobile_screen_layout.dart';
 import 'package:google_clone/responsive/responsive_layout_screen.dart';
 import 'package:google_clone/responsive/web_screen_layout.dart';
-import 'package:google_clone/screens/search_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,11 +19,10 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: backgroundColor,
       ),
-      // home: const ResponsiveLayoutScreen(
-      //   mobileScreenLayout: MobileScreenLayout(),
-      //   webScreenLayout: WebScreenLayout(),
-      // ),
-      home: const SearchScreen(),
+      home: const ResponsiveLayoutScreen(
+        mobileScreenLayout: MobileScreenLayout(),
+        webScreenLayout: WebScreenLayout(),
+      ),
     );
   }
 }
--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -7,7 +7,13 @@ import 'package:google_clone/widgets/search_result_component.dart';
 import 'package:google_clone/widgets/search_tabs.dart';
 
 class SearchScreen extends StatelessWidget {
-  const SearchScreen({super.key});
+  const SearchScreen({
+    super.key,
+    required this.searchQuery,
+    required this.start,
+  });
+  final String searchQuery;
+  final String start;
 
   @override
   Widget build(BuildContext context) {
@@ -28,15 +34,19 @@ class SearchScreen extends StatelessWidget {
               height: 0,
               thickness: 0.3,
             ),
-            // search result
+            // showing search result
             FutureBuilder(
-              future: ApiService().fetchData(queryTerm: 'Name'),
+              future: ApiService().fetchData(
+                queryTerm: searchQuery,
+                start: start,
+              ),
               builder: (BuildContext context, AsyncSnapshot snapshot) {
                 if (snapshot.hasData) {
                   return Column(
                     mainAxisAlignment: MainAxisAlignment.start,
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
+                      // showing the time it took to fetch results
                       Container(
                         padding: const EdgeInsets.only(left: 150, top: 12),
                         child: Text(
@@ -47,6 +57,7 @@ class SearchScreen extends StatelessWidget {
                           ),
                         ),
                       ),
+                      // displaying the results
                       ListView.builder(
                         itemCount: snapshot.data?['items'].lenght,
                         shrinkWrap: true,
@@ -64,6 +75,65 @@ class SearchScreen extends StatelessWidget {
                           );
                         },
                       ),
+                      const SizedBox(height: 30),
+                      // pagination buttons
+                      SizedBox(
+                        width: double.infinity,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            TextButton(
+                              onPressed: () {
+                                // if start is 0, we are on the first page
+                                if (start != '0') {
+                                  Navigator.of(context).push(
+                                    MaterialPageRoute(
+                                      builder: (context) => SearchScreen(
+                                        searchQuery: searchQuery,
+                                        start:
+                                            (int.parse(start) - 10).toString(),
+                                      ),
+                                    ),
+                                  );
+                                }
+                              },
+                              child: const Text(
+                                '< Prev',
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  color: blueColor,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(width: 30),
+                            TextButton(
+                              onPressed: () {
+                                if (start != '0') {
+                                  Navigator.of(context).push(
+                                    MaterialPageRoute(
+                                      builder: (context) => SearchScreen(
+                                        searchQuery: searchQuery,
+                                        start:
+                                            (int.parse(start) + 10).toString(),
+                                      ),
+                                    ),
+                                  );
+                                }
+                              },
+                              child: const Text(
+                                'Next >',
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  color: blueColor,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(height: 30),
+                      const SearchFooter(),
                     ],
                   );
                 }
@@ -72,39 +142,6 @@ class SearchScreen extends StatelessWidget {
                 );
               },
             ),
-            // pagination buttons
-            SizedBox(
-              width: double.infinity,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  TextButton(
-                    onPressed: () {},
-                    child: const Text(
-                      '< Prev',
-                      style: TextStyle(
-                        fontSize: 15,
-                        color: blueColor,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(width: 30),
-                  TextButton(
-                    onPressed: () {},
-                    child: const Text(
-                      'Next >',
-                      style: TextStyle(
-                        fontSize: 15,
-                        color: blueColor,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 30),
-            const SearchFooter(),
           ],
         ),
       ),
